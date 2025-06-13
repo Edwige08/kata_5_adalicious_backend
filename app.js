6cbfec2619dd292bec2ae90c1a82d71c0ddf5838
Edwige08 <edwige.saves@gmail.com>
2025-06-13 15:40:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+require('dotenv').config()
+
+const dishesRoutes = require('./routes/dishes')
+
+app.use(cors());
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const port = 3010;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/dishes', dishesRoutes)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+require('dotenv').config();
+
+const { Pool } = require('pg');
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+        require: true,
+    },
+});
+
+async function getPgVersion() {
+    const client = await pool.connect();
+    try {
+        const result = await client.query('SELECT version()');
+        console.log(result.rows[0]);
+    } finally {
+        client.release();
+    }
+}
+
+getPgVersion();
